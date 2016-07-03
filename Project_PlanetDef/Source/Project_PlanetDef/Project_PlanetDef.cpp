bca6a36a4caff5bd7a6e540fcd5e432b4ebb53f3
TrogdorIII <joshua.glavich@gmail.com>
2016-07-03 21:06:37 +1000

    Different meteorite types + unfinished physics simulation

---
+++ Project_PlanetDef/Source/Project_PlanetDef/Project_PlanetDef.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project_PlanetDef.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_PlanetDef, "Project_PlanetDef" );
